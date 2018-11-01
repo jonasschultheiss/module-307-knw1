fed17742a7f08d83b79cc7ca1a6a49deffe17e28
quest1onmark <jonas.schultheiss3@gmail.com>
2018-11-01 20:30:26 +0100

    init

---
+++ produkte_fleisch_rind.php
@@ -0,0 +1,9 @@
+<?php
+//Seiten Parameter setzen
+$page_titel = "Bio Rindfleisch ab Hof";
+$page_kurztitel = "Rind";
+$page_news = true;
+
+//Template includen
+include("includes/template.inc.php");
+?>
